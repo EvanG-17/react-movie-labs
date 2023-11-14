2e40621ff1d2d98458431023b313086ba1d38b2f
Evan Geary <etg1709@gmail.com>
2023-11-14 13:33:18 +0000

    Added routing and live data from API.

--- movies/src/components/headerMovie/index.js
+++ movies/src/components/headerMovie/index.js
@@ -5,7 +5,7 @@ import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
-import MovieDetailsPage from './pages/movieDetailsPage'
+
 
 const MovieHeader = (props) => {
   const movie = props.movie;
